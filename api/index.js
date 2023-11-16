1f09e1e719edc03a9966da30b235e1bb0043d026
Tariq Horan <N00202437@iadt.ie>
2023-11-16 21:10:01 +0000

    hosting test

--- api/index.js
+++ api/index.js
@@ -1,7 +1,60 @@
-const app = require("express")();
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = 3000;
+const morgan = require("morgan");
+
+require("../configs/db.js")();
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  let token = null;
+  if (req.headers.authorization) {
+    token = req.headers.authorization.split(" ");
+  }
+  console.log(token);
+  if (token && token[0] === "Bearer") {
+    // Verify token is valid
+    jwt.verify(token[1], process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        console.log("Token verification error:", err);
+      } else {
+        req.user = decoded;
+      }
+      next();
+    });
+  } else {
+    console.log("No token");
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  console.log(req.user);
+  next();
+});
+
+app.use(express.static(__dirname + "/public/"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello");
+});
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/workoutssplits", require("./routes/workoutssplits"));
+app.use("/api/workoutsexercises", require("./routes/workoutsexercises"));
+app.use("/api/exercises", require("./routes/exercises"));
+app.use("/api/workouts", require("./routes/workouts"));
+app.use("/api/splits", require("./routes/splits"));
+app.use("/api/userData", require("./routes/userdata"));
+
+app.listen(port, () => {
+  console.log(`listening at port, ${port}`);
 });
 
 module.exports = app;
