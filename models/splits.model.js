427599825e0fe77b89619090657949bc2df25424
Tariq Horan <N00202437@iadt.ie>
2023-11-28 22:44:50 +0000

    changing splits model

--- models/splits.model.js
+++ models/splits.model.js
@@ -4,13 +4,27 @@ const splitsSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, "Name of the splits is required"],
+      required: [true, "Name of the split is required"],
       unique: true,
+    },
+    workout: {
+      type: [Schema.Types.ObjectId],
+      required: [true, "Name of the workout is required"],
+      ref: "Workout",
+    },
+    notes: {
+      type: [String],
+    },
+    dateStart: {
+      type: Date,
+    },
+    dateEnd: {
+      type: Date,
     },
     user: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: [true, "Name of the splits is required"],
+      required: [true, "Name of the User is required"],
     },
   },
   { timestamps: true }
