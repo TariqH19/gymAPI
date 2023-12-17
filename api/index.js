d04d8efe275fdc3099501439d0151a45bc13fcd1
Tariq Horan <N00202437@iadt.ie>
2023-12-17 00:38:46 +0000

    Update static file serving path in API

--- api/index.js
+++ api/index.js
@@ -92,7 +92,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(__dirname + "/public/"));
+app.use("/public", express.static(__dirname + "/public/"));
 
 app.use("/api/users", require("../routes/users"));
 app.use("/api/weights", require("../routes/weights"));
