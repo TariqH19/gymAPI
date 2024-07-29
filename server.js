5d1b5a9ea8310aa9a3b461b51a7cfd0fe7a809e2
TariqH19 <tariqhoran88@gmail.com>
2024-07-29 10:14:10 +0100

    subs

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const app = express();
 const port = 3000;
 const cors = require("cors");
@@ -12,12 +11,114 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.use((req, res, next) => {
-  console.log(req.user);
-  next();
-});
+const generateAccessToken = async () => {
+  try {
+    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+      throw new Error("MISSING_API_CREDENTIALS");
+    }
+    const auth = Buffer.from(
+      `${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`
+    ).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "POST",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error("Failed to generate Access Token:", error);
+    throw error;
+  }
+};
 
-app.use(express.static(__dirname + "/public/"));
+app.post("/api/create-product-plan", async (req, res) => {
+  try {
+    const accessToken = await generateAccessToken();
+    const productResponse = await fetch(`${base}/v1/catalogs/products`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Gym Membership",
+        description: "A monthly membership for the gym",
+        type: "SERVICE",
+        category: "EXERCISE_AND_FITNESS",
+        image_url: "https://example.com/image.jpg",
+        home_url: "https://example.com",
+      }),
+    });
+
+    if (!productResponse.ok) {
+      const error = await productResponse.text();
+      console.error("Product creation error:", error);
+      return res.status(productResponse.status).send("Error creating product");
+    }
+
+    const productData = await productResponse.json();
+    const productId = productData.id;
+
+    const planResponse = await fetch(`${base}/v1/billing/plans`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        product_id: productId,
+        name: "Basic Gym Plan",
+        description: "Monthly subscription to the gym.",
+        billing_cycles: [
+          {
+            frequency: {
+              interval_unit: "MONTH",
+              interval_count: 1,
+            },
+            tenure_type: "REGULAR",
+            sequence: 1,
+            total_cycles: 12,
+            pricing_scheme: {
+              fixed_price: {
+                value: "35",
+                currency_code: "GBP",
+              },
+            },
+          },
+        ],
+        payment_preferences: {
+          auto_bill_outstanding: true,
+          setup_fee: {
+            value: "0",
+            currency_code: "GBP",
+          },
+          setup_fee_failure_action: "CONTINUE",
+          payment_failure_threshold: 3,
+        },
+        taxes: {
+          percentage: "0",
+          inclusive: false,
+        },
+      }),
+    });
+
+    if (!planResponse.ok) {
+      const error = await planResponse.text();
+      console.error("Plan creation error:", error);
+      return res.status(planResponse.status).send("Error creating plan");
+    }
+
+    const planData = await planResponse.json();
+    res.json({ productId, planId: planData.id });
+  } catch (error) {
+    console.error("Error creating product or plan:", error);
+    res.status(500).send("Error creating product or plan");
+  }
+});
 
 app.use("/api/cart", require("./routes/cart.js"));
 app.use("/api/custom", require("./routes/custom.js"));
